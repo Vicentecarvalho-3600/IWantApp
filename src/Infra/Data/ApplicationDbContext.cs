f356d93b64cb8312015bd8f549cb772d4126eadc
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-06-26 19:11:42 -0300

    gerando e aplicando migrations

--- src/Infra/Data/ApplicationDbContext.cs
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -19,6 +19,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
         builder.Ignore<Notification>();
 
         builder.Entity<Product>()
