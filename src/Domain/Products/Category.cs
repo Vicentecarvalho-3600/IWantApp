3a696fb07d389089f726bd6f30eee4d7a9a1eb5b
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-06-26 18:50:01 -0300

    utilizando metodos de extensão

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -4,22 +4,36 @@ namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; }
-    public bool Active { get; set; }
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
     public Category(string name, string createdBy, string editedBy)
     {
-        var contract = new Contract<Category>()
-            .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório")
-            .IsGreaterOrEqualsThan(name, 3, "Name", "O Nome precisa ter 3 ou mais caracteres")
-            .IsNotNullOrEmpty(createdBy, "CreatedBy", "CreateBy é obrigatório")
-            .IsNotNullOrEmpty(editedBy, "EditedBy", "EditedBy é obrigatório");
-        AddNotifications(contract);
-
         Name = name;
         Active = true;
         CreatedBy = createdBy;
         EditedBy = editedBy;
         CreatedOn = DateTime.Now;
         EditedOn = DateTime.Now;
+
+        Validate();
+
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Category>()
+                    .IsNotNullOrEmpty(Name, "Name", "Nome é obrigatório")
+                    .IsGreaterOrEqualsThan(Name, 3, "Name", "O Nome precisa ter 3 ou mais caracteres")
+                    .IsNotNullOrEmpty(CreatedBy, "CreatedBy", "CreateBy é obrigatório")
+                    .IsNotNullOrEmpty(EditedBy, "EditedBy", "EditedBy é obrigatório");
+        AddNotifications(contract);
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validate();
     }
 }
