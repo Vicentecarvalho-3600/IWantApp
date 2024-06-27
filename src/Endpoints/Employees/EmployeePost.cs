84b693dc526b11dede02bba908340d8ded285ec3
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-06-27 01:25:47 -0300

    retornando error com problem details

--- src/Endpoints/Employees/EmployeePost.cs
+++ src/Endpoints/Employees/EmployeePost.cs
@@ -1,6 +1,4 @@
-﻿using IWantApp.Domain.Products;
-using IWantApp.Infra.Data;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
 
@@ -16,8 +14,8 @@ public class EmployeePost
         var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
         var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
 
-        if(!result.Succeeded)
-            return Results.BadRequest(result.Errors.First());
+        if (!result.Succeeded)
+            return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
 
         var userClaims = new List<Claim>
         {
