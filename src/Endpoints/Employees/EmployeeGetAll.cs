58e300de98fa900d7c668bac4b491743e187bc96
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-06-29 20:33:56 -0300

    utilizando dapper para melhorar a performace

--- src/Endpoints/Employees/EmployeeGetAll.cs
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.Security.Claims;
-
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -9,18 +8,23 @@ public class EmployeeGetAll
     public static string Template => "/employees";
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
-    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    public static IResult Action(int? page, int? rows, IConfiguration configuration)
     {
-        var users = userManager.Users.Skip(( page - 1) * rows).Take(rows).ToList();
-        var employees = new List<EmployeesResponse>();
-        foreach (var item in users) 
-        {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null ? claimName.Value : string.Empty;
-            employees.Add(new EmployeesResponse(item.Id, userName));
-        }
+        if (page == null)
+            return Results.BadRequest("necessario informar a page"); // procurar melhorar
 
-        return Results.Ok(employees);
+        if (rows == null)
+            return Results.BadRequest("necessario informar a rows"); // procurar melhorar
+
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query = @"select Email, ClaimValue as Name
+                    from AspNetUsers u inner join AspNetUserClaims c
+                    on u.Id = c.UserId and claimtype = 'Name'
+                    order by Name
+                    offset (@page - 1) * @rows rows fetch next @rows rows only";
+
+        var employees = db.Query<EmployeesResponse>(query, new {page, rows});
+
+        return Results.Ok( employees );
     }
 }
